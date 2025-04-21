a202ce789ee2aebf4d0658f6b10c01cc7928d931
Ananth Subramanya <71940967+ams29@users.noreply.github.com>
2025-04-21 09:29:14 -0700

    test
    test

---
+++ carrierlogistics/mobile/snletlive/BuildConfig.java
@@ -0,0 +1,9 @@
+package carrierlogistics.mobile.snletlive;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "carrierlogistics.mobile.snletlive";
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final int VERSION_CODE = 20000003;
+    public static final String VERSION_NAME = "2.0.3";
+}
